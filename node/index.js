a08c0ddc72420329948b1ef291626359be37b69f
Felipe Carvalho <felipee_sc@hotmail.com>
2021-03-07 11:11:19 -0300

    challenge nginx with node using proxy revers

--- node/index.js
+++ node/index.js
@@ -5,20 +5,38 @@ const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database:'nodedb'
+    database: 'nodedb'
 };
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Felipe Carvalho')`
-const search = `SELECT * FROM people;`
-connection.query(sql)
-connection.end()
+const create_table = 'CREATE TABLE IF NOT EXISTS people (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+connection.query(create_table)
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+const sql = `INSERT INTO people(name) values('Felipe Carvalho')`
+connection.query(sql)
+
+const select = 'SELECT name FROM people'
+connection.query(select)
+
+let listaNome = ''
+connection.query(select, (_err, result, _) => {
+    let i
+    for (i = 0; i < result.length; i++) {
+        listaNome += '<li>' + result[i].name + '</li>'
+    }
 })
 
-app.listen(port, ()=> {
+connection.end()
+
+app.get('/', (req, res) => {
+    let template = '<h1>Full Cycle Rocks!</h1></br>'
+    template += '<ul>'
+    template += listaNome
+    template += '</ul>'
+    res.send(template)
+})
+
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
 })
